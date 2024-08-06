8b7a0369c881da4a19ffcef461e32f1ebad77d82
Tyler Boyd <ty@tyboyd.dev>
2024-08-06 16:12:48 -0500

    adding all sorts of good stuff

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,58 +1,63 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "gopls", "svelte", "tailwindcss" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "tsserver", "gopls", "svelte", "tailwindcss" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- lsp keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            -- lsp keymaps
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-			-- lua
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+            -- lua
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
 
-			-- typescript/javascript
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
+            -- typescript/javascript
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
 
             -- tailwindcss
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+            })
 
-			-- gopls
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+            -- gopls
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
 
-			-- svelte
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-			})
+            -- svelte
+            lspconfig.svelte.setup({
+                capabilities = capabilities,
+            })
 
-			-- docker
-			lspconfig.dockerls.setup({
-				capabilities = capabilities,
-			})
-		end,
-	},
+            -- docker
+            lspconfig.dockerls.setup({
+                capabilities = capabilities,
+            })
+
+            -- zig
+            lspconfig.zls.setup({
+                capabilities = capabilities,
+            })
+        end,
+    },
 }
